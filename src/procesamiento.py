1ae1d984937f80f1714a17327689da9f2482f25f
Ulices1Chingo <ulices.chingo@yachaytech.edu.ec>
2025-10-07 19:58:38 -0500

    procesamiento.py

--- src/procesamiento.py
+++ src/procesamiento.py
@@ -1,1 +1,13 @@
 import pandas as pd
+from IPython.display import display
+
+def cargar_datos(path):
+  """
+  Función para cargar datos desde un archivo CSV o Excel.imp
+  Input:
+    path (str): Ruta del archivo a cargar
+  Output:
+    data (DataFrame): Datos cargados en un DataFrame de pandas.
+  """
+  try:
+    
