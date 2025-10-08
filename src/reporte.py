ce013cb3ad77cfaaf80eed093307c03f1e6f640a
SofiaFeijooC <162819200+SofiaFeijoo03@users.noreply.github.com>
2025-10-08 17:08:01 -0500

    Update reporte.py

--- src/reporte.py
+++ src/reporte.py
@@ -1,10 +1,14 @@
 import pandas as pd
-from datetime import datetime
+import os
+from IPython.display import display
 
 def val_anormales(totDATA):
     """
     Función para identificar y mostrar días con valores anormales en las horas trabajadas.
-    
+    Input:
+        totDATA (DataFrame): DataFrame con el tiempo total trabajado por empleado por día.
+    Output:
+        None: Muestra en pantalla los días con valores anormales.
     """
     #Identificar valores anormales
     for i in range(len(totDATA["Minutos extra 50%"])):
@@ -13,6 +17,14 @@ def val_anormales(totDATA):
             display(totDATA.loc[i])
 
 def generar_reporte(totDATA, file_name = "reporte_horas_extras.csv"):
+    """
+    Función para generar y guardar un reporte de horas extras por empleado.
+    Input:
+        totDATA (DataFrame): DataFrame con el tiempo total trabajado por empleado por día.
+        file_name (str, optional): Nombre del archivo CSV donde se guardará el reporte. Por defecto es "reporte_horas_extras.csv".
+    Output:
+        None: Guarda el reporte en un archivo CSV.
+    """
     listado = totDATA["Nombre"].unique()
     tot50 = [0 for k in range(len(listado))]
     tot100 = [0 for k in range(len(listado))]
@@ -24,3 +36,30 @@ def generar_reporte(totDATA, file_name = "reporte_horas_extras.csv"):
                 tot100[j] += totDATA.loc[i, "Minutos extra 100%"]
                 dias[j] +=1
     reporte = pd.DataFrame({"Nombre": listado, "Total minutos extra 50%": tot50, "Total minutos extra 100%": tot100})
+
+    #Tranformar a horas (decimal format) y añadir días asistidos
+    hor50 = []
+    hor100 = []
+    for i in range(len(reporte["Nombre"])):
+        h5 = reporte.loc[i, "Total minutos extra 50%"] / int(60)
+        hor50.append(h5)
+        h1 = reporte.loc[i, "Total minutos extra 100%"] / int(60)
+        hor100.append(h1)
+    reporte_final = pd.DataFrame({"Nombre": listado, "Total horas extras 50%": hor50, "Total horas extras 100%": hor100, "Dias asistidos": dias})
+    reporte_final.sort_values(by='Total horas extras 50%')
+
+    # Crear un directorio en donde se guardará el reporte
+    dir_output = "output_data"
+    if os.path.isdir(dir_output):
+        print(f"Directorio '{dir_output}' ya existe.")
+    else:
+        print(f"Directorio '{dir_output}' ha sido creado.")
+        os.mkdir(dir_output)
+    # Gurdar el reporte
+    try:
+        reporte_final.to_csv(dir_output + "/" + file_name, index=False)
+        print(f"Reporte guardado exitosamente en {dir_output + '/' + file_name}")
+    except Exception as e:
+        print(f"Error al guardar el reporte: {e}")
+
+    return reporte_final
